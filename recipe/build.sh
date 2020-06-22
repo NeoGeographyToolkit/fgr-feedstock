3ed2b5d3d7d3ea35701782fbd3096ba0afc048e4
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2020-06-22 11:41:49 -0700

    Adjust source location

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-cd source
 mkdir build && cd build
 
-# Make libraries
+CUSTOM_SOURCE_DIR=${SRC_DIR}/source
+
+# Build
 cmake                                          \
   -DCMAKE_BUILD_TYPE=Release                   \
   -DCMAKE_CXX_FLAGS='-g -O3 -std=c++11'        \
@@ -11,14 +12,14 @@ cmake                                          \
   -DCMAKE_PREFIX_PATH=${PREFIX}                \
   -DCMAKE_VERBOSE_MAKEFILE=ON                  \
   -DFastGlobalRegistration_LINK_MODE=SHARED    \
-  ${SRC_DIR}
+  ${CUSTOM_SOURCE_DIR}
 
 make -j $CPU_COUNT
 
 # Install
 INC_DIR=${PREFIX}/include/FastGlobalRegistration
 mkdir -p ${INC_DIR}
-cp -fv ${SRC_DIR}/FastGlobalRegistration/app.h ${INC_DIR}
+cp -fv ${CUSTOM_SOURCE_DIR}/FastGlobalRegistration/app.h ${INC_DIR}
 LIB_DIR=${PREFIX}/lib
 mkdir -p ${LIB_DIR}
 cp -fv FastGlobalRegistration/libFastGlobalRegistrationLib* ${LIB_DIR}
