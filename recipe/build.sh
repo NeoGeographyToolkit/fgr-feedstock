824150a3d1bbc3a708cbebdcf9fa9e385e055128
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2020-07-02 17:56:44 -0700

    Turn off debug flags

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,13 +5,13 @@ mkdir build && cd build
 CUSTOM_SOURCE_DIR=${SRC_DIR}/source
 
 # Build
-cmake                                          \
-  -DCMAKE_BUILD_TYPE=Release                   \
-  -DCMAKE_CXX_FLAGS='-g -O3 -std=c++11'        \
-  -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX}        \
-  -DCMAKE_PREFIX_PATH=${PREFIX}                \
-  -DCMAKE_VERBOSE_MAKEFILE=ON                  \
-  -DFastGlobalRegistration_LINK_MODE=SHARED    \
+cmake                                        \
+  -DCMAKE_BUILD_TYPE=Release                 \
+  -DCMAKE_CXX_FLAGS='-O3 -std=c++11'         \
+  -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX}      \
+  -DCMAKE_PREFIX_PATH=${PREFIX}              \
+  -DCMAKE_VERBOSE_MAKEFILE=ON                \
+  -DFastGlobalRegistration_LINK_MODE=SHARED  \
   ${CUSTOM_SOURCE_DIR}
 
 make -j $CPU_COUNT
