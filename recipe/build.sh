049c5f81c5e73eb9078d1bf2ecb751043ce3fccc
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-04-25 17:50:24 -0700

    Standardize how compilers are used

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,14 +5,24 @@ mkdir build && cd build
 CUSTOM_SOURCE_DIR=${SRC_DIR}/source
 INC_FLAGS="-I${PREFIX}/include/eigen3 -I${PREFIX}/include -O3 -L${PREFIX}/lib -lflann_cpp -O3 -std=c++11"
 
-# Build
-cmake                                        \
-  -DCMAKE_BUILD_TYPE=Release                 \
-  -DCMAKE_CXX_FLAGS="${INC_FLAGS}"           \
-  -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX}      \
-  -DCMAKE_PREFIX_PATH=${PREFIX}              \
-  -DCMAKE_VERBOSE_MAKEFILE=ON                \
-  -DFastGlobalRegistration_LINK_MODE=SHARED  \
+if [ "$(uname)" = "Darwin" ]; then
+    cc_comp=clang
+    cxx_comp=clang++
+else
+    cc_comp=x86_64-conda-linux-gnu-gcc
+    cxx_comp=x86_64-conda-linux-gnu-c++
+fi
+
+# Enforce a compiler we know to work
+cmake                                          \
+  -DCMAKE_C_COMPILER=${PREFIX}/bin/$cc_comp    \
+  -DCMAKE_CXX_COMPILER=${PREFIX}/bin/$cxx_comp \
+  -DCMAKE_BUILD_TYPE=Release                   \
+  -DCMAKE_CXX_FLAGS="${INC_FLAGS}"             \
+  -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX}        \
+  -DCMAKE_PREFIX_PATH=${PREFIX}                \
+  -DCMAKE_VERBOSE_MAKEFILE=ON                  \
+  -DFastGlobalRegistration_LINK_MODE=SHARED    \
   ${CUSTOM_SOURCE_DIR}
 
 make -j $CPU_COUNT
